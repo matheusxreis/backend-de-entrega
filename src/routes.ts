68a6ebef4fc11f354c72f9216bdb08a6c46cf443
matheusxreis <matheus.reis081@gmail.com>
2022-02-01 16:18:35 -0300

    CreateDeliverymanUseCase e Controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express'
 import { AuthenticateClientController } from './modules/account/authenticateClient/authenticateClientController'
 import { CreateClientController } from './modules/clients/useCases/createClient/createClientController'
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController'
 
 const routes = Router()
 
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
-
+const createDeliverymanController = new CreateDeliverymanController()
 
 routes.post('/client', createClientController.handle)
 routes.post('/authenticate', authenticateClientController.handle)
+routes.post('/deliveryman', createDeliverymanController.handle)
 
 // No express, o que for passado após minha rota, funciona automaticamente como um middleware.
 // Por tanto, passando apenas o controler, o request e response já irão automaticamente pra ele
